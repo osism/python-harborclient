abf027f9421f6692923dd492f511a55d9ac09451
Christian Berendt <berendt@osism.tech>
2023-06-23 13:58:52 +0200

    Autoformat python syntax with black
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- robot_accounts.py
+++ robot_accounts.py
@@ -3,20 +3,20 @@ from api import Api
 
 def rework_permissions(permissions_list: list) -> list:
     for permission in permissions_list:
-        permission['access'] = []
-        for entry in permission['access_simplified']:
+        permission["access"] = []
+        for entry in permission["access_simplified"]:
             resource, action = entry.split(":")
-            permission['access'].append(
-                {"action": action, "resource": resource}
-            )
-        del permission['access_simplified']
+            permission["access"].append({"action": action, "resource": resource})
+        del permission["access_simplified"]
     return permissions_list
 
 
 def create(api: Api, robot_accounts: list) -> list:
     result = []
     for robot_account in robot_accounts:
-        robot_account['permissions'] = rework_permissions(permissions_list=robot_account['permissions'])
+        robot_account["permissions"] = rework_permissions(
+            permissions_list=robot_account["permissions"]
+        )
 
         rvalue = api.post(endpoint="robots", payload=robot_account)
 
